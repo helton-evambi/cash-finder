c0839b48fde26ed758e843167a8f5000315e12ed
Helton Evambi <heltonevambi@gmail.com>
2024-05-22 19:56:52 +0100

    configure routes

--- App.tsx
+++ App.tsx
@@ -1,26 +1,24 @@
 import { Image, SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { Logo } from './assets'
 import { CustomButton, CustomInput } from './components'
 import AtmCard from './components/atm-card'
+import { NavigationContainer } from '@react-navigation/native'
+import Login from './screen/auth/login'
 
-const Login = () => {
+const Stack = createNativeStackNavigator()
+export default function App() {
   return (
-    <SafeAreaView style={styles.body}>
-      <AtmCard />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={Login}
+          options={{ headerShown: false }}
+        />
+        {/* <Stack.Screen name="Details" component={DetailsScreen} /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
-
-export default Login
-
-const styles = StyleSheet.create({
-  body: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    gap: 8,
-    paddingTop: 54,
-    paddingHorizontal: 24,
-  },
-})
