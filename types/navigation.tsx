25a03f4402f5c106df7349473d32fdc9af81c711
Helton Evambi <heltonevambi@gmail.com>
2024-05-24 02:29:12 +0100

    create types to navigation

---
+++ types/navigation.tsx
@@ -0,0 +1,7 @@
+export type RootStackParamList = {
+  Home: undefined
+  Login: undefined
+  Contribuir: undefined
+  Pesquisar: undefined
+  AtmList: { query: 'all' | 'money' | 'paper' | 'system' }
+}
