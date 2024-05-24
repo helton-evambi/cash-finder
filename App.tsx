f3f9accc5126bdf22e7974d005cbc600797ccb2e
Helton Evambi <heltonevambi@gmail.com>
2024-05-24 01:09:01 +0100

    update routing

--- App.tsx
+++ App.tsx
@@ -7,6 +7,9 @@ import AtmCard from './components/atm-card'
 import { NavigationContainer } from '@react-navigation/native'
 import Login from './screen/auth/login'
 import Home from './screen/home'
+import Contribuir from './screen/contribuir/contribuir'
+import Pesquisar from './screen/pesquisa/pesquisa'
+import AtmsList from './screen/atms-list/atm-list'
 
 const Stack = createNativeStackNavigator()
 export default function App() {
@@ -18,7 +21,26 @@ export default function App() {
           component={Home}
           options={{ headerShown: false }}
         />
-        {/*<Stack.Screen name="Details" component={DetailsScreen} />*/}
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Contribuir"
+          component={Contribuir}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Pesquisar"
+          component={Pesquisar}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="AtmList"
+          component={AtmsList}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   )
